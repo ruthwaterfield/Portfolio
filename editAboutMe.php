3cce407d03d1c33f3f29377861e8c789ead24611
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-10-02 13:21:33 +0100

    showing data from database in editing page

--- editAboutMe.php
+++ editAboutMe.php
@@ -1,33 +1,85 @@
 <?php
-$db = new PDO('mysql:host=127.0.0.1;dbname=WebsitePrototypeDb', 'root', '');
-$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "INSERT INTO `adults` (`name`, `DOB`, `gender`) VALUES (:name, :DOB, :gender);";
-$query = $db -> prepare($sql);
 
-if(isset($_POST['name']) && $_POST['DOB'] && $_POST['gender']) {
-    $outputString = 'Added an adult with ';
-    $query->bindParam(':name', $_POST['name'], PDO::PARAM_STR, 255);
-    $outputString .= ' name: ' . $_POST['name'];
-    $query->bindParam(':DOB', $_POST['DOB'], PDO::PARAM_STR, 10);
-    $outputString .= ', Date of Birth: ' . $_POST['DOB'];
-    if($_POST['gender']=='m' || $_POST['gender']=='f') {
-        $query->bindParam(':gender', $_POST['gender'], PDO::PARAM_STR, 1);
-        $outputString .= ', Gender: ' . $_POST['gender'];
-    }
-    else {
-        $gender = NULL;
-        $query->bindParam(':gender', $gender,PDO::PARAM_NULL);
-    }
+/**
+ * getContentName returns the first text_content field in the TextContents table of the database
+ *
+ * @return string the text content
+ */
+function getTextContent() : string {
     try {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=WebsitePrototypeDb', 'root', '');
+        $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT `text_content` FROM `TextContents` LIMIT 1;";
+        $query = $db -> prepare($sql);
+
         $query->execute();
-        echo $outputString;
+        $returnedArray = $query->fetch();
+        $result =$returnedArray["text_content"];
     }
     catch(Exception $e) {
-        echo $e->getMessage();
+        $result = $e->getMessage();
     }
 
+    return $result;
 }
+
+
+
+/**
+ * getContentName returns the first text_content field in the TextContents table of the database
+ *
+ * @return string the text content
+ */
+function getContentName() : string {
+    try {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=WebsitePrototypeDb', 'root', '');
+        $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT `content_name` FROM `TextContents` LIMIT 1;";
+        $query = $db -> prepare($sql);
+
+        $query->execute();
+        $returnedArray = $query->fetch();
+        $result =$returnedArray["content_name"];
+    }
+    catch(Exception $e) {
+        $result = $e->getMessage();
+    }
+
+    return $result;
+}
+
+
+
+//$sql = "INSERT INTO `adults` (`name`, `DOB`, `gender`) VALUES (:name, :DOB, :gender);";
+//$query = $db -> prepare($sql);
+//
+//if(isset($_POST['name']) && $_POST['DOB'] && $_POST['gender']) {
+//    $outputString = 'Added an adult with ';
+//    $query->bindParam(':name', $_POST['name'], PDO::PARAM_STR, 255);
+//    $outputString .= ' name: ' . $_POST['name'];
+//    $query->bindParam(':DOB', $_POST['DOB'], PDO::PARAM_STR, 10);
+//    $outputString .= ', Date of Birth: ' . $_POST['DOB'];
+//    if($_POST['gender']=='m' || $_POST['gender']=='f') {
+//        $query->bindParam(':gender', $_POST['gender'], PDO::PARAM_STR, 1);
+//        $outputString .= ', Gender: ' . $_POST['gender'];
+//    }
+//    else {
+//        $gender = NULL;
+//        $query->bindParam(':gender', $gender,PDO::PARAM_NULL);
+//    }
+//    try {
+//        $query->execute();
+//        echo $outputString;
+//    }
+//    catch(Exception $e) {
+//        echo $e->getMessage();
+//    }
+//
+//}
 
 ?>
--- editAboutMeForm.php
+++ editAboutMeForm.php
@@ -1,4 +1,4 @@
-<?php require_once editAboutMe.php ?>
+<?php require_once('editAboutMe.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,16 +8,15 @@
 <body>
 <h1> Edit About Me</h1>
 
-<form method="POST" action="EditAboutMe.php">
+
+<form method="POST" action="editAboutMe.php">
     Content Name:
     <label title="Content Name:"/>
-    <input name="content_name" type="text" required maxlength="20" value=
-    <?php echo  ?>
-    <br/>
+    <input name="content_name" type="text" required maxlength="20" value="<?php echo getContentName()?>"> <br/>
     Text Content:
     <label title="Text Content"/>
-    <input name="text_content" type="date" required> <br/>
-    <input type="Update">
+    <input name="text_content" type="text" required value="<?php echo getTextContent()?>"> <br/>
+    <input type="submit" value="Update">
 </form>
 
 </body>
