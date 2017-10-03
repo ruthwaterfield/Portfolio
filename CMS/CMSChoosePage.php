f197e33c9b64edec73800059121f6f3a5829c696
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-10-03 09:52:57 +0100

    added navigation to CMSChoosePage and renamed consistently to CMS..

---
+++ CMS/CMSChoosePage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Choose page </title>
+</head>
+
+<h1> Choose page to edit: </h1>
+
+<nav>
+    <ul>
+        <li>
+            <a href="CMShome.php">
+                HOME
+            </a>
+        </li>
+        <li>
+            <a href="CMSaboutMe.php">
+                ABOUT ME
+            </a>
+        </li>
+        <li>
+            <a href="CMSportfolio.php">
+                PORTFOLIO
+            </a>
+        </li>
+        <li>
+            <a href="CMScontactMe.php">
+                CONTACT ME
+            </a>
+        </li>
+    </ul>
+</nav>
+
+
+
+</html>
+
