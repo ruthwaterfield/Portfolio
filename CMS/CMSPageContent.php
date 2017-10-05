9455fa7c59dad3a67236fc88e9044bbcbd5493e6
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-10-05 22:12:34 +0100

    added style for CMSPageContent

--- CMS/CMSPageContent.php
+++ CMS/CMSPageContent.php
@@ -1,20 +1,16 @@
-<?php require_once ('CMSNavigation.php');
-require_once ('CMSPageContentFunctions.php');
+<?php require_once('CMSNavigation.php');
+require_once('CMSPageContentFunctions.php');
 
 session_start();
 if ($_SESSION['loggedIn'] != 1) {
     header('Location: index.php');
 }
 
-if(isset($_GET['pageId']))
-{
+if (isset($_GET['pageId'])) {
     $_SESSION['pageId'] = $_GET['pageId'];
-}
-else if(isset($_SESSION['pageId'])) {
+} else if (isset($_SESSION['pageId'])) {
     //page already chosen
-}
-else
-{
+} else {
     header('Location: CMSChoosePage.php');
 }
 ?>
@@ -22,61 +18,77 @@ else
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Page content: <?php echo getPageNameFromPageId($_SESSION['pageId'])?>  </title>
+    <title>Page content: <?php echo getPageNameFromPageId($_SESSION['pageId']) ?>  </title>
+    <link rel="stylesheet" type="text/css" href="../CMSStyles/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../CMSStyles/CMSTheme.css">
 </head>
 
 <body>
-<h1> Page content: <?php echo getPageNameFromPageId($_SESSION['pageId'])?></h1>
 
-<form action="CMSLogIn.php" method="POST">
-    <input type="submit" name="logOut" value="LOG OUT">
-</form>
+<header>
+    <h1> Page content: <?php echo getPageNameFromPageId($_SESSION['pageId']) ?></h1>
 
-<div>
-<a href="CMSChoosePage.php">
-    Choose a different page
-</a>
-</div>
+    <form action="CMSLogIn.php" method="POST">
+        <input type="submit" name="logOut" value="Log out">
+    </form>
 
-<div>
-
-    <a href="CMSAddContentForm.php">
-        Add content
+    <a href="CMSChoosePage.php">
+        Choose a different page
     </a>
+</header>
 
-<div>
+<div class="mainContent">
 
-<h3>Existing content</h3>
-    <div>
-
-        <?php
-        $pageContentsArray = getContentForPage($_SESSION['pageId']);
-        foreach ($pageContentsArray as $contentItem) { ?>
-            <div>
-            <p>
-                Text label: <?php echo $contentItem['textLabel']?>
+    <?php
+    $pageContentsArray = getContentForPage($_SESSION['pageId']);
+    foreach ($pageContentsArray as $contentItem) { ?>
+        <div class="contentItem">
+            <p class="label">
+                Text label:
             </p>
-            <p>
-                Text content: <?php echo $contentItem['textContent']?>
+            <p class="content">
+                <?php echo $contentItem['textLabel'] ?>
             </p>
-                <?php if (contentHasImage($contentItem['imageLabel'], $contentItem['imageLocation'])) { ?>
-                    <p>
-                        Image label: <?php echo $contentItem['imageLabel']?>
-                    </p>
-                    <p>
-                        Image location: <?php echo $contentItem['imageLocation']?>
-                    </p>
+            <p class="label">
+                Text content:
+            </p>
+            <p class="content">
+                <?php echo $contentItem['textContent'] ?>
+            </p>
+            <?php if (contentHasImage($contentItem['imageLabel'], $contentItem['imageLocation'])) { ?>
+                <p class="label">
+                    Image label:
+                </p>
+                <p class="content">
+                    <?php echo $contentItem['imageLabel'] ?>
+                </p>
+                <p class="label">
+                    Image location:
+                </p>
+                <p class="content">
+                    <?php echo $contentItem['imageLocation'] ?>
+                </p>
+            <?php } ?>
 
-                <?php } ?>
-
-                <a href="CMSEditContentForm.php?id=<?php echo $contentItem['id'];?>">
-                    Edit/Delete this item
-                </a>
+            <div class="divider">
             </div>
+            <div class="buttonDiv">
+            <a href="CMSEditContentForm.php?id=<?php echo $contentItem['id']; ?>">
+                Edit/Delete this item
+            </a>
+            </div>
+        </div>
         <?php
-        }
-        ?>
+    }
+    ?>
 
-</div>
+    <div class="contentItem">
+        <div class="buttonDiv">
+        <a href="CMSAddContentForm.php">
+            Add content
+        </a>
+    </div>
+    </div>
+
 
 </body>
